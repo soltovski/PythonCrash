3096a1f3a4a0f0cf352902f5b42de02ede18dded
Soltovski <rodrigo.soltovski@gmail.com>
2022-06-03 18:45:24 -0300

    1.5: Pág 245 - Running the Game in Fullscreen Mode

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,8 +12,12 @@ class AlienInvasion:
         pygame.init() #inicializa o background
 
         self.settings = Settings()#cria um objeto do tipo settings
+
+
         self.screen = pygame.display.set_mode(
-             (self.settings.screen_width,self.settings.screen_height))#cria e define o tamanho da tela
+             (0,0),pygame.FULLSCREEN)#cria e define o tamanho da tela
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion") #define o nome
 
         self.ship = Ship(self)
