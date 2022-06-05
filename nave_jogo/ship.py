a795745e0300367d7de628bdaf97be3119838422
Soltovski <rodrigo.soltovski@gmail.com>
2022-06-05 08:47:34 -0300

    10: Pág 241 - Adjusting the Ship’s Speed

--- nave_jogo/ship.py
+++ nave_jogo/ship.py
@@ -10,6 +10,7 @@ class Ship:
         self.tela = parametro.tela # tela_ship recebe 'tela' de nave_jogo
         self.tela_rect = parametro.tela.get_rect() # atribui o rect da tela
                                     # de nave_jogo à tela_ship_rect
+        self.settings = parametro.settings #
 
         #carrega a imagem da nave e pega seu rect
         self.imagem_nave = pygame.image.load('imagens/ship.bmp')#carrega imagem
@@ -19,6 +20,8 @@ class Ship:
         self.ship_rect.midbottom = self.tela_rect.midbottom #o rect inferior central
                                     # da figura da nave será o mesmo do rect inferior
                                     # central da tela_ship
+        #grava um valor decimal para a posição horizontal da nave
+        self.pos_x=float(self.ship_rect.x)
 
         #flag tecla continuamente pressionada
         self.vai_para_dir = False
@@ -26,10 +29,14 @@ class Ship:
 
     def update(self):
         '''atualiza a posição da nave basedo na flag'''
+        #atualiza o valor da posição decimal x, não do rect
         if self.vai_para_dir:
-            self.ship_rect.x += 1 #movimenta a figura da nave para a direita
+            self.pos_x += self.settings.veloc_ship #movimenta a figura da nave para a direita
         if self.vai_para_esq:
-            self.ship_rect.x -= 1   #movimenta a figura da nave para a esquerda
+            self.pos_x -= self.settings.veloc_ship   #movimenta a figura da nave para a esquerda
+
+        #Atualiza o rect a partir de pos_x
+        self.ship_rect.x = self.pos_x
 
     def blitme(self):
         '''faz aparecer a imagem da nave na posicao indicada'''
